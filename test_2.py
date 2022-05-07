cbef62295ee6faacda7d4ad9170213e4ede265aa
Pheng <phengyangster9@gmail.com>
2022-05-06 22:14:19 -0700

    finished structured-english

--- test_2.py
+++ test_2.py
@@ -7,10 +7,8 @@ Secondly, write a caluculate_percentage_above_average() function to
 calculate the percentage of grades that are aboce the average grade.
 
 Function 1 will kickstart the application, will extract from
-the Final.txt fil and close after the information has been collected.
-Function 2 named average grade will collect the information from
-Final.txt and will use the average function to average it.
-Function 3 named percentage of grades above average will calculate
+the Final.txt file and calculate the average grade and close after the information has been collected.
+Function 2 named percentage of grades above average will calculate
 the percentage of grades that are above the average grade.
 """
 
